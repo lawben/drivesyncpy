0ece3f5937e28ce6982df8082a08080771f80f4b
lawrencebenson <lawrence.benson@student.hpi.de>
2015-12-22 14:13:17 +0100

    Add PathConverter for relative paths in Event

--- drivesyncpy/sync_drive.py
+++ drivesyncpy/sync_drive.py
@@ -1,6 +1,9 @@
 import sys
+from os.path import relpath
 
 import pyinotify
+
+from drive_connector import DriveConnector
 
 
 INOTIFY_EVENT_MASK = pyinotify.IN_CREATE | pyinotify.IN_MODIFY | \
@@ -8,22 +11,36 @@ INOTIFY_EVENT_MASK = pyinotify.IN_CREATE | pyinotify.IN_MODIFY | \
                      pyinotify.IN_MOVED_TO
 
 
+class _PathConverter(pyinotify.ProcessEvent):
+    def my_init(self, root_dir):
+        self._root_dir = root_dir
+
+    def process_default(self, event):
+        setattr(event, "converted_path", self._convert_path(event))
+
+    def _convert_path(self, event):
+        return relpath(event.pathname, self._root_dir)
+
+
 class UpSyncWatcher(pyinotify.ProcessEvent):
-    def my_init(self, watchManager):
-        self._wm = watchManager
+    def my_init(self, watch_manager, drive_connector):
+        self._wm = watch_manager
+        self._dc = drive_connector
 
     def process_IN_MODIFY(self, event):
         print("IN_MODIFY {}".format(event))
+        self._dc.upload_file(event.converted_path)
 
     def process_IN_CREATE(self, event):
         print("IN_CREATE {}".format(event))
         if event.dir:
-            print("Started watching {}".format(event.pathname))
-            self._wm.add_watch(event.pathname, INOTIFY_EVENT_MASK)
-        print(self._wm.watches)
+            print("Started watching {}".format(event.converted_path))
+            self._wm.add_watch(event.converted_path, INOTIFY_EVENT_MASK)
+        self._dc.upload_file(event.converted_path)
 
     def process_IN_DELETE(self, event):
         print("IN_DELETE {}".format(event))
+        self._dc.delete_file(event.converted_path)
 
     def process_IN_MOVED_FROM(self, event):
         print("MOVED FROM {}".format(event))
@@ -33,6 +50,7 @@ class UpSyncWatcher(pyinotify.ProcessEvent):
             print("MOVED TO: {} with {}".format(event.src_pathname, event))
         else:
             print("create new {}".format(event))
+            self._dc.upload_file(event.converted_path)
 
     def process_default(self, event):
         print("Default Event: {}".format(event))
@@ -40,7 +58,12 @@ class UpSyncWatcher(pyinotify.ProcessEvent):
 
 def sync_drive(root_dir):
     wm = pyinotify.WatchManager()
-    event_handler = UpSyncWatcher(watchManager=wm)
+    dc = DriveConnector()
+    path_converter = _PathConverter(root_dir=root_dir)
+    event_handler = UpSyncWatcher(path_converter,
+                                  watch_manager=wm,
+                                  drive_connector=dc)
+
     notifier = pyinotify.Notifier(wm, default_proc_fun=event_handler)
     wm.add_watch(root_dir, INOTIFY_EVENT_MASK)
     notifier.loop()
